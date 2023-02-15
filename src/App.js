91b6b57732df8b15b4d2fc87f511a2a06c605161
yusuf-mulla <124673307+yusuf-mulla@users.noreply.github.com>
2023-02-16 01:19:24 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,13 @@
-// import logo from './logo.svg';
-import './App.css';
-<<<<<<< HEAD
-import { Counter } from './Component2/Counter/Counter';
-import { Yusuf } from './Component2/Yusuf';
-=======
-<<<<<<< HEAD
-import { Sufiyan } from './componets3/Sufiyan';
-=======
-<<<<<<< HEAD
-import { Yusuf } from './Component2/Yusuf';
-=======
-import { Khuzi } from './Component1/Khuzi';
->>>>>>> a56df6d496786e3e5b5059372a328702c53fce47
->>>>>>> 8724239d26819a8f0bbccf165aa345c53e49a5dd
->>>>>>> 1799380bb29415c9e031bbffc89a46798c99e278
-import { TodoCompo } from './TodoCompo';
+import './App.css'
+import {yusuf} from './Component2/Yusuf'
+import {Counter} from './Component2/Counter/Counter'
 
 function App() {
-  return (
-    <div className="App">
-      {/* <TodoCompo /> */}
-<<<<<<< HEAD
-      {/* <Yusuf/> */}
-      <Counter/>
-=======
-<<<<<<< HEAD
-      <Ibrahim/>
-      {/* <h1>This is h1 tag</h1> */}
-=======
-<<<<<<< HEAD
-      <Sufiyan/>
-      
-=======
-<<<<<<< HEAD
-      <Yusuf/>
-=======
-      {/* <h1>This is h1 tag</h1> */}
-      <Khuzi/>
->>>>>>> a56df6d496786e3e5b5059372a328702c53fce47
->>>>>>> 8724239d26819a8f0bbccf165aa345c53e49a5dd
->>>>>>> 30cd3619e95477ae6a8ff2351c02307e5d9ff774
->>>>>>> 1799380bb29415c9e031bbffc89a46798c99e278
-    </div>
-  );
-}
+return (
+      <div className='App'>
 
+            <Counter/>
+      </div>
+)      
+}
 export default App;
