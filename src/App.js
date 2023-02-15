a56df6d496786e3e5b5059372a328702c53fce47
khuzi-shaikh <124673301+khuzi-shaikh@users.noreply.github.com>
2023-02-15 11:40:06 +0530

    khuzaim's file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
+import { Khuzi } from './Component1/Khuzi';
 import { TodoCompo } from './TodoCompo';
 
 function App() {
   return (
     <div className="App">
-      <TodoCompo />
-      <h1>This is h1 tag</h1>
+      {/* <TodoCompo /> */}
+      {/* <h1>This is h1 tag</h1> */}
+      <Khuzi/>
     </div>
   );
 }
